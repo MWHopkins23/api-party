73ac7ebd95580cdd7ad551faaf7222819eab1555
MWHopkins23 <mwhopkin19@wabash.edu>
2017-05-30 11:36:04 -0400

    Fixed the Github search bar

--- src/Github.js
+++ src/Github.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import './Github.css'
+import { Route } from 'react-router-dom'
+import GithubUser from './GithubUser.js'
 
 class Github extends Component {
     state = {
@@ -11,23 +13,36 @@ class Github extends Component {
         this.setState({ username })
     }
 
+    handleSubmit = (ev) => {
+        ev.preventDefault()
+        this.props.history.push(`/github/${this.state.username}`)
+
+    }
+
     render() {
         return (
-            <div className="github">
-                <img className="github-logo" src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="github" />
-                <form>
+            <div className = "github">
+                <img className = "github-logo" src = "http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt = "github" />
+                <form onSubmit = {this.handleSubmit}>
                     <div>
-                        <input type="text" 
-                            value={this.state.username} 
-                            onChange={this.handleChange} />
+                        <input 
+                        type = "text" 
+                        value = {this.state.username}
+                        onChange = {this.handleChange} />
                     </div>
-                    <div>
-                        <button type="submit">Look up a Github user</button>
-                    </div>
+                <div>
+                    <button type = "submit"> Look up Github user </button>
+                </div>
                 </form>
+
+                <Route exact path = '/github' render = {() => <h3> Please enter a username to search on Github </h3>} />
+                <Route path = '/github/:username' component = { GithubUser } />
             </div>
+            
+
         )
     }
+
 }
 
 export default Github
