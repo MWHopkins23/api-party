e256f2460bdc4fbae190460bcec61c78f077c592
MWHopkins23 <mwhopkin19@wabash.edu>
2017-05-30 15:00:12 -0400

    Premier League API

--- src/ClubInfo.js
+++ src/ClubInfo.js
@@ -10,16 +10,30 @@ class ClubInfo extends Component {
         }
     }
 
-    componentWillMount = () => {
-    this.fetchClubData(this.props)
-  }
+//     componentWillMount = () => {
+//     this.fetchClubData(this.props)
+//   }
 
-  componentWillReceiveProps(nextProps) {
-    const locationChanged = nextProps.location !== this.props.location;
-    if (locationChanged) {
-      this.fetchRoverData(nextProps);
-    }
-  }
+//   componentWillReceiveProps(nextProps) {
+//     const locationChanged = nextProps.location !== this.props.location;
+//     if (locationChanged) {
+//       this.fetchRoverData(nextProps);
+//     }
+//   }
+
+//   fetchClubData(props) {
+//     fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${props.match.params.rover}/photos?earth_date=${photoDates[props.match.params.rover]}&camera=fhaz&api_key=DEMO_KEY`)
+//       .then(res => res.json())
+//       .then(data => {
+//         const rover = {
+//           imageUrl: data.photos[0].club.name,
+//           location: data.photos[0].img_src,
+//           yearFounded: data.photos[0].earth_date
+//         }
+//         this.setState({club})
+//       })
+//       .catch(err => console.warn(err))
+//   }
     
     render () {
         return (
